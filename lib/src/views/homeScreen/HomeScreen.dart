865ff89e4eb6f795ae27a64beb09e188577ca3d0
Mustafa Kaya <mustafakaya.developer@gmail.com>
2024-03-14 16:30:03 +0200

    Home Screen | Score table changed according to instructor information.

--- lib/src/views/homeScreen/HomeScreen.dart
+++ lib/src/views/homeScreen/HomeScreen.dart
@@ -1,24 +1,70 @@
 import 'package:capstone/src/views/homeScreen/widgets/CustomBottomSheet.dart';
 import 'package:capstone/src/views/homeScreen/widgets/ScoreTable.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
   static const String routeName = "/homeScreen";
+
   @override
   Widget build(BuildContext context) {
+    String archerName = "Mustafa Kaya"; //Gerçek okçu adı ile değiştir.
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("Welcome Archer"),
+        title: const Text("Hoşgeldin Okçu"),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/archer.jpeg'),
+            ),
+          ),
+        ],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
-        children: [Expanded(child: ScoreTable()), AddScoreBottomSheet()],
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 10.0, bottom: 10.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  archerName,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: const [
+                ScoreTable(
+                  allScores: [
+                    [2, 5, 6, 4, 8, 6],
+                    [3, 6, 7, 5, 9, 7],
+                    [2, 5, 6, 4, 8, 6],
+                    [3, 6, 7, 5, 9, 7],
+                    [2, 5, 6, 4, 8, 6],
+                    [3, 6, 7, 5, 9, 7],
+                    [2, 5, 6, 4, 8, 6],
+                    [3, 6, 7, 5, 9, 7],
+                    [2, 5, 6, 4, 8, 6],
+                    [3, 6, 7, 5, 9, 7],
+                    [2, 5, 6, 4, 8, 6],
+                    [3, 6, 7, 5, 9, 7]
+                  ],
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+          ),
+          const AddScoreBottomSheet(),
+          const SizedBox(height: 16),
+        ],
       ),
     );
   }
--- lib/src/views/homeScreen/widgets/ScoreTable.dart
+++ lib/src/views/homeScreen/widgets/ScoreTable.dart
@@ -1,42 +1,80 @@
-import 'package:capstone/src/views/profileScreen/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-import '../../../util/textStyles.dart';
-import 'ScoreTableHeader.dart';
+class ScoreTableCell extends StatelessWidget {
+  final String text;
 
-class ScoreTable extends StatelessWidget {
-  const ScoreTable({super.key});
+  const ScoreTableCell({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Score Table",
-            style: tableHeaderStyle,
-          ),
-          Table(
-              border: TableBorder.all(),
-              columnWidths: const <int, TableColumnWidth>{
-                0: FlexColumnWidth(),
-                1: FlexColumnWidth(),
-                2: FlexColumnWidth(),
-              },
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: <TableRow>[
-                TableRow(
-                  children: <Widget>[
-                    ScoreTableHeader("Archer id"),
-                    ScoreTableHeader("Archer Full Name"),
-                    ScoreTableHeader("Score"),
-                  ],
-                ),
-              ]),
-        ],
+      alignment: Alignment.center,
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 16.0),
+      ),
+    );
+  }
+}
+
+class ScoreTable extends StatelessWidget {
+  final List<List<int>> allScores;
+
+  const ScoreTable({super.key, required this.allScores});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      color: Colors.grey[300],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Table(
+          border: TableBorder.all(),
+          columnWidths: const <int, TableColumnWidth>{
+            0: FlexColumnWidth(),
+            1: FlexColumnWidth(),
+            2: FlexColumnWidth(),
+            3: FlexColumnWidth(),
+            4: FlexColumnWidth(),
+            5: FlexColumnWidth(),
+            6: FlexColumnWidth(),
+          },
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: <TableRow>[
+            TableRow(
+              children: <Widget>[
+                const ScoreTableCell(text: ""),
+                for (int i = 0; i < 6; i++)
+                  ScoreTableCell(text: "Atış ${i + 1}"),
+                const ScoreTableCell(text: "Toplam"),
+              ],
+            ),
+            for (int i = 0; i < allScores.length; i++)
+              TableRow(
+                children: <Widget>[
+                  ScoreTableCell(text: "Set ${i + 1}"),
+                  for (int j = 0; j < 6; j++)
+                    ScoreTableCell(text: "${allScores[i][j]}"),
+                  ScoreTableCell(
+                      text: "${allScores[i].reduce((value, element) => value + element)}"),
+                ],
+              ),
+            TableRow(
+              children: <Widget>[
+                const ScoreTableCell(text: "Toplam"),
+                for (int j = 0; j < 6; j++)
+                  const ScoreTableCell(text: ""),
+                ScoreTableCell(
+                    text: "${allScores.expand((scores) => scores).reduce((value, element) => value + element)}"),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
