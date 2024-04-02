4ecc60054f64c024443e648c57b00e5344a2e0e9
kayamustafa33 <mustafakaya.developer@gmail.com>
2024-04-02 19:01:05 +0300

    Models | Models created.

---
+++ lib/src/models/Elimination.dart
@@ -0,0 +1,8 @@
+class Elimination {
+  int scoreId;
+  int playerId;
+  int competitionId;
+  int totalScore;
+
+  Elimination({required this.scoreId, required this.playerId, required this.competitionId, required this.totalScore});
+}
---
+++ lib/src/models/Season.dart
@@ -0,0 +1,8 @@
+class Season {
+  int seasonId;
+  int playerId;
+  int position;
+  int totalScore;
+
+  Season({required this.seasonId, required this.playerId, required this.position, required this.totalScore});
+}
