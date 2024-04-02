6a32690e62dbdbda7a13d788773ce26e88f47a02
kayamustafa33 <mustafakaya.developer@gmail.com>
2024-04-02 19:01:40 +0300

    Models | Models created.

---
+++ lib/src/models/Competition.dart
@@ -0,0 +1,10 @@
+class Competition {
+  int competitionId;
+  String competitionName;
+  String competitionStyle;
+  DateTime competitionDate;
+  String compAgeGroup;
+  String location;
+
+  Competition({required this.competitionId, required this.competitionName, required this.competitionStyle, required this.competitionDate, required this.compAgeGroup, required this.location});
+}
